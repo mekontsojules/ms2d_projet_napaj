1364808c925f757ddaefc97f621fc68d2dbc4e1e
mekontso jules auffred <mekontsj@3il.fr>
2023-10-30 15:35:46 +0100

    changement de template et ajout du listing des ingredients

---
+++ Pages/Ingredient/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ms2dNapaj.Pages.Ingredient
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Ingredient/Index.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ms2dNapaj.DAL;
+using Ms2dNapaj.Models;
+
+namespace Ms2dNapaj.Pages.Ingredient
+{
+    public class IndexModel : PageModel
+    {
+		private readonly NapajDBContext _context;
+
+		public IndexModel(NapajDBContext context)
+		{
+			_context = context;
+		}
+
+		public List<Ms2dNapaj.Models.Ingredient> Ingredients { get; set; }
+		public string ErrorMessage { get; set; }
+
+		public void OnGet()
+		{
+			try
+			{
+				Ingredients = _context.Ingredients.Include(i=>i.Supplier).ToList();
+			}
+			catch (Exception ex)
+			{
+				Ingredients = new List<Ms2dNapaj.Models.Ingredient>(); // Initialisation d'une liste vide en cas d'échec
+				ErrorMessage = "Une erreur s'est produite lors de la récupération des ingrédients. Veuillez réessayer.";
+			}
+		}
+	}
+}

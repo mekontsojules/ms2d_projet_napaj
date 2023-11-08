df29243d36bce7a4577e237c467b2962ed5488bf
mekontso jules auffred <mekontsj@3il.fr>
2023-11-08 11:23:07 +0100

    login and register

--- Models/ProductCatalog.cs
+++ Models/ProductCatalog.cs
@@ -6,6 +6,9 @@
         public List<Recipe> Recipes { get; set; }
         public DateTime GenerationDate { get; set; } = DateTime.Now;
         public string PdfFileUrl { get; set; }
+
+        //soutenance le 22 decembre
+        //75% de marge
         //presenter les ingredeint et les prixs
     }
 }
